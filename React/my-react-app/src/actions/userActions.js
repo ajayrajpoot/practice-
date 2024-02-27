d2a0542bbb360c481519cfd2cfcc212193bd838a
Ajay Singh Rajpoot <ajayrajpoot1993@gmail.com.com>
2024-02-27 07:45:18 +0530

    add redux

---
+++ React/my-react-app/src/actions/userActions.js
@@ -0,0 +1,8 @@
+ 
+// userActions.js
+export const setUser = (user) => {
+  return {
+    type: 'SET_USER',
+    payload: user
+  };
+};
--- React/my-react-app/src/components/Home.js
+++ React/my-react-app/src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Counter from './components/Counter';
+import User from './components/User';
+
 function Home(){
-    return( <h1>Home</h1>)
+    return(<>
+    <h1>Home</h1>
+    <Counter />
+        <User />
+
+    </> )
 }
 
 export default Home;
